5ad64a3fe262b70f3da9e04c06cb16635c566010
Harold Jimenez <harold3112@gmail.com>
2026-02-22 17:55:46 -0400

    Numero de telefono real

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -49,7 +49,7 @@ export default function Footer() {
               </li>
               <li className="flex items-center gap-3">
                 <Phone size={18} className="text-brand-gold shrink-0" />
-                <span>+1 (809) 555-0199</span>
+                <span>+1 (809) 661-4214</span>
               </li>
               <li className="flex items-center gap-3">
                 <Mail size={18} className="text-brand-gold shrink-0" />
